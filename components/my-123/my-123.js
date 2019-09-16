b71f50b1fab6d2c1b9dd155ee769c0dcf7117439
ligl521 <18353077779@163.com>
2019-09-16 09:48:41 +0800

    190916-1组件

---
+++ components/my-123/my-123.js
@@ -0,0 +1,13 @@
+// components/my-123/my-123.js
+Component({
+  properties:{ 
+    titel:{
+      type:String,
+      value:"默认"
+    }
+  },
+  data:{
+    tt:"11123"
+  },
+  externalClasses:["titleclass"]
+});
